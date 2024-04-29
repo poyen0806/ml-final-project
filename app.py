e2e51b0c003c011b356d514bde9b895a8ba4814a
poyen0806 <leo33917888@gmail.com>
2024-04-29 16:29:54 +0800

    add: 新增flask基礎code

---
+++ app.py
@@ -0,0 +1,16 @@
+# import modules
+from flask import Flask, render_template, request
+
+# create an instance of the Flask class
+app = Flask(__name__)
+
+# define routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# add new route here
+
+# run the app
+if __name__ == '__main__':
+    app.run(debug=True)
